cb457cbd790e20ea234f0ad1831f06632eaae0cc
carlewis <carlewis@06edc168-1444-0410-8887-8bc0c0194c32>
2008-01-22 12:51:54 +0000

    Primera etapa de la negociacion de lider, falta enviar los mensajes agente por agente y el bloqueo hasta recibir las respuestas

--- trunk/JGomas/src/student/MyComponents.java
+++ trunk/JGomas/src/student/MyComponents.java
@@ -1,24 +1,37 @@
 package student;
-//import es.upv.dsic.gti_ia.jgomas.CTroop;
 
 public class MyComponents {
 	//private CTroop thisTroop;
 
 	/**
 	 * Tipos de Agentes
-	 * TODO: Mover a la clase MyComponents 
 	 */
 	public enum AgentType { SOLDIER, MEDIC, FIELDOPS };
+	/**
+	 * 
+	 * @author carlos
+	 *
+	 */
+	public enum LeaderMessage { REQUEST, ALREADY_EXISTS, I_WIN, YOU_WIN };
+	/**
+	 * 
+	 * @param t: string
+	 * @return AgentType
+	 */
 	public static AgentType parseAgentType(String t) {
 		if (t.equals("SOLDIER")) return AgentType.SOLDIER;
 		if (t.equals("MEDIC")) return AgentType.MEDIC;
 		if (t.equals("FIELDOPS")) return AgentType.FIELDOPS;
 		return AgentType.SOLDIER;
 	}
-	
-	
-	//public MyComponents(CTroop agent) {
-	//	thisTroop = agent;
-	//}
+	public static LeaderMessage parseLeaderMessage(String t) {
+		if (t.equals("REQUEST")) return LeaderMessage.REQUEST;
+		if (t.equals("I_WIN")) return LeaderMessage.I_WIN;
+		if (t.equals("YOU_WIN")) return LeaderMessage.YOU_WIN;
+		if (t.equals("ALREADY_EXISTS")) return LeaderMessage.ALREADY_EXISTS;
+		return LeaderMessage.REQUEST;
+	}
 	
 }
+
+
